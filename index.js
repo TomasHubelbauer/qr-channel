2e06530b996cdf6eacc2a43b2c79662c544328bc
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-23 16:49:32 +0100

    Switch to an actual Google STUN server not the example one

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ window.addEventListener('load', async () => {
     requestAnimationFrame(tick);
   });
   
-  const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stuns:stun.example.org' } ] });
+  const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun.l.google.com:19302' } ] });
   for (const key in peerConnection) {
     if (!/^on/.test(key)) {
       continue;
