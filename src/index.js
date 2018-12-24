da8b565b120df6934268c701f77dfdde8436b39d
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 10:40:22 +0100

    Try to make Chrome work by moving camera work to a gesture handler

--- src/index.js
+++ src/index.js
@@ -6,17 +6,12 @@ import monitor from './monitor.js';
 import test from './test.js';
 
 window.addEventListener('load', async () => {
+  await test();
+  
   const signalingStateP = document.querySelector('#signalingStateP');
   const iceGatheringStateP = document.querySelector('#iceGatheringStateP');
   const chunksP = document.querySelector('#chunksP');
   
-  const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
-  viewfinderVideo.srcObject = mediaStream;
-  // Set this attribute (not class member) through JavaScript (not HTML) to make iOS Safari work
-  viewfinderVideo.setAttribute('playsinline', true);
-  // Play within the `getUserMedia` gesture, `autoplay` won't work
-  await viewfinderVideo.play();
-
   const chunks = [];
   scan((index, count, text) => {
     chunks[index] = text;
@@ -29,26 +24,49 @@ window.addEventListener('load', async () => {
     }
   });
   
-  const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.services.mozilla.com' } ] });
-  monitor(peerConnection, 'peerConnection');
-  
-  peerConnection.addEventListener('signalingstatechange', () => signalingStateP.textContent += peerConnection.signalingState + '; ');
-  peerConnection.addEventListener('icegatheringstatechange', () => iceGatheringStateP.textContent += peerConnection.iceGatheringState + '; ');
-  
-  const dataChannel = peerConnection.createDataChannel(null);
-  monitor(dataChannel, 'dataChannel');
-      
-  const sessionDescription = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(sessionDescription);
+  const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+  viewfinderVideo.srcObject = mediaStream;
+  // Set this attribute (not class member) through JavaScript (not HTML) to make iOS Safari work
+  viewfinderVideo.setAttribute('playsinline', true);
+  // Play within the in gesture if needed (Chrome), `autoplay` won't work
+  try {
+    await viewfinderVideo.play();
+    await offer();
+  } catch (error) {
+    alert('Click the page to begin');
+    // Retry in a gesture handler to make Chrome work
+    document.body.addEventListener('pointerdown', _ => {
+      try {
+        await viewfinderVideo.play();
+        await offer();
+      } catch (error) {
+        alert('Error');
+      }
+    });
+  }
 
-  // Start rotating now that we have SDP
-  broadcast(peerConnection);
+  async function offer() {
+    const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.services.mozilla.com' } ] });
+    monitor(peerConnection, 'peerConnection');
+
+    peerConnection.addEventListener('signalingstatechange', () => signalingStateP.textContent += peerConnection.signalingState + '; ');
+    peerConnection.addEventListener('icegatheringstatechange', () => iceGatheringStateP.textContent += peerConnection.iceGatheringState + '; ');
+
+    const dataChannel = peerConnection.createDataChannel(null);
+    monitor(dataChannel, 'dataChannel');
+
+    const sessionDescription = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(sessionDescription);
+
+    // Start rotating now that we have SDP
+    broadcast(peerConnection);
+  }
   
   // TODO: Rewrite this to be able to act on partial messages so that collection of chunks and establishment of the connection do not have to be strictly sequential
   async function connect(sdp) {
     const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.services.mozilla.com' } ] });
     monitor(peerConnection, 'peerConnection');
-    
+
     switch (sdp.type) {
       case 'offer': {
         break;
@@ -63,8 +81,6 @@ window.addEventListener('load', async () => {
 
     // TODO: Finalize this logic
   }
-  
-  await test();
 });
 
 window.addEventListener('unhandledrejection', event => alert(event.reason));
