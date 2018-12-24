ca7fb328779e57a4989b65480d3c4a9f0f212a55
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 22:00:58 +0100

    Get rid of the chunking indication as we're not using chunks now

--- src/scan.js
+++ src/scan.js
@@ -2,9 +2,7 @@
 export default function scan(onMessage) {
   const viewfinderVideo = document.querySelector('#viewfinderVideo');
   const viewfinderCanvas = document.querySelector('#viewfinderCanvas');
-  const chunksP = document.querySelector('#chunksP');
   let viewfinderContext;
-  const chunks = [];
   window.requestAnimationFrame(function recognize() {
     if (viewfinderVideo.readyState === viewfinderVideo.HAVE_ENOUGH_DATA) {
       if (viewfinderCanvas.width !== viewfinderVideo.videoWidth || viewfinderCanvas.height !== viewfinderVideo.videoHeight) {
@@ -17,22 +15,7 @@ export default function scan(onMessage) {
       const imageData = viewfinderContext.getImageData(0, 0, viewfinderCanvas.width, viewfinderCanvas.height);
       const code = jsQR(imageData.data, imageData.width, imageData.height);
       if (code !== null && code.data !== '') {
-        const [indexStr, countStr, text] = code.data.split('\0', 3);
-        const index = Number(indexStr);
-        const count = Number(countStr);
-        if (chunks[index] === undefined) {
-          chunks[index] = text;
-          // Check the number of keys to ignore uninitialized array items
-          if (Object.keys(chunks).length === count) {
-            const message = chunks.join('');
-            onMessage(message);
-          }
-        } else if (chunks[index] !== text) {
-          // Reset scanning as we've found different chunks at the same index, the peer has probably changed
-          chunks = [];
-        }
-        
-        chunksP.innerHTML = Array(count).map(i => `<input type='checkbox' disabled${chunks[i] !== undefined ? 'checked' : ''} />`).join('');
+        onMessage(code.data);
       }
     }
 
