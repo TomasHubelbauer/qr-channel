510d639367c3ce9efd1ac77aa3c34bf6cb2f1fbd
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 16:41:53 +0100

    Implement decoding the data bits from the value string

--- src/decode.js
+++ src/decode.js
@@ -1,24 +1,46 @@
 // Decodes SDP + ICE candidates from a QR alphanumeric string
 // TODO: Finalize decompressing and unescaping
-export default function decode(data) {
+export default function decode(value) {
+  let type;
+  let media;
+  switch (value[0]) {
+    case 'O': type = 'offer'; media = 'firefox'; break;
+    case 'P': type = 'offer'; media = 'chrome'; break;
+    case 'A': type = 'answer'; media = 'firefox'; break;
+    case 'B': type = 'answer'; media = 'chrome'; break;
+  }
+  
+  // TODO: Read a new bit which says whether the ICE candidates are lowercased or mixedcased
+  const hashRaw = value.slice(1, 1 + 64); // TODO: Insert the colons
+  let hash = '';
+  for (let index = 0; index < hashRaw.length / 2; index++) {
+    hash += hashRaw.slice(index * 2, index * 2 + 2) + ':';
+  }
+
+  hash = hash.slice(0, -1);
+  const id = value.slice(1 + 64, 1 + 64 + 19);
+  const ufrag = value.slice(1 + 64 + 19, value.indexOf(':')).toLowerCase();
+  const pwd = value.slice(value.indexOf(':') + ':'.length).toLowerCase();
+  
+  console.log({ type, media, id, hash, ufrag, pwd });
   return new RTCSessionDescription({
-    type: data.type,
+    type: type,
     sdp: [
       'v=0',
-      `o=- ${data.id} 0 IN IP4 0.0.0.0`,
+      `o=- ${id} 0 IN IP4 0.0.0.0`,
       's=-',
       't=0 0',
-      `a=fingerprint:sha-256 ${data.hash}`,
+      `a=fingerprint:sha-256 ${hash}`,
       `a=group:BUNDLE 0`,
       'a=ice-options:trickle',
       'a=msid-semantic:WMS',
-      `m=application 9 ${data.media === 'firefox' ? 'UDP/DTLS/SCTP webrtc-datachannel' : ''}${data.media === 'chrome' ? 'DTLS/SCTP 5000' : ''}`,
+      `m=application 9 ${media === 'firefox' ? 'UDP/DTLS/SCTP webrtc-datachannel' : ''}${media === 'chrome' ? 'DTLS/SCTP 5000' : ''}`,
       'c=IN IP4 0.0.0.0',
-      `a=ice-ufrag:${data.ufrag}`,
-      `a=ice-pwd:${data.pwd}`,
+      `a=ice-ufrag:${ufrag}`,
+      `a=ice-pwd:${pwd}`,
       'a=mid:0',
-      `a=setup:${data.type === 'offer' ? 'actpass' : ''}${data.type === 'answer' ? 'active' : ''}`,
-      `a=sctp${data.media === 'firefox' ? '-port:5000' : ''}${data.media === 'chrome' ? 'map:5000 webrtc-datachannel 1024' : ''}`,
+      `a=setup:${type === 'offer' ? 'actpass' : ''}${type === 'answer' ? 'active' : ''}`,
+      `a=sctp${media === 'firefox' ? '-port:5000' : ''}${media === 'chrome' ? 'map:5000 webrtc-datachannel 1024' : ''}`,
       '',
     ].join('\r\n'),
   });
