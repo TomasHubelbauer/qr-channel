a68c7ec5eafdd0fa6b30f29ccfaf5bdf5ea3119f
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 10:34:38 +0100

    Send the true message to the connection logic

--- src/index.js
+++ src/index.js
@@ -23,8 +23,9 @@ window.addEventListener('load', async () => {
     chunksP.textContent = `Chunks (${Object.keys(chunks).length}/${count}): ` + Object.keys(chunks);
     // Check the number of keys to ignore uninitialized array items
     if (Object.keys(chunks).length === count) {
+      const message = chunks.join('');
       // Fire and forget
-      connect();
+      connect(decode(message));
     }
   });
   
@@ -43,34 +44,24 @@ window.addEventListener('load', async () => {
   // Start rotating now that we have SDP
   broadcast(peerConnection);
   
-  // TODO: Handle both offer and answer cases
   // TODO: Rewrite this to be able to act on partial messages so that collection of chunks and establishment of the connection do not have to be strictly sequential
-  async function connect() {
+  async function connect(sdp) {
     const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.services.mozilla.com' } ] });
     monitor(peerConnection, 'peerConnection');
     
-    const dataChannel = peerConnection.createDataChannel('');
-    monitor(dataChannel, 'dataChannel');
- 
-    const message = chunks.join('');
-    const parts = message.split('\0');
-    
-    // TODO: Handle this possible being an answer to own offer and not different offer
-    const offer = new RTCSessionDescription({ type: parts[0], sdp: parts[1] });
-    await peerConnection.setRemoteDescription(offer);
-    
-    // TODO: Parse candidates from the groups of three items
-    //for (const ) {
-    //  const candidate = new RTCIceCandidate({ candidate: iceString, sdpMid: 0, sdpMLineIndex: 0 });
-    //  await peerConnection.addIceCandidate(candidate);
-    //}
-    
-    // TODO: Verify only a blank item was left (confirming we're at the end of the candidates)
-    
-    //const answer = await peerConnection.createAnswer();
-    //await peerConnection.setLocalDescription(answer);
-    
-    alert('done');
+    switch (sdp.type) {
+      case 'offer': {
+        break;
+      }
+      case 'answer': {
+        break;
+      }
+      default: {
+        throw new Error(`Unexpected SDP type '${sdp.type}'.`);
+      }
+    }
+
+    // TODO: Finalize this logic
   }
   
   await test();
