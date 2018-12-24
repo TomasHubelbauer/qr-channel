fb5a527926384cc7da5bf14cd994690790ed36ad
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 11:29:35 +0100

    Push logs to their own paragraph with white space breaking

--- src/monitor.js
+++ src/monitor.js
@@ -1,4 +1,5 @@
 const mode = 'body';
+let logsP;
 export default function monitor(obj, label) {
   for (const key in obj) {
     if (!/^on/.test(key)) {
@@ -7,10 +8,25 @@ export default function monitor(obj, label) {
 
     obj.addEventListener(key.slice(2), event => {
       switch (mode) {
-        case 'log': console.log(label, key, event); break;
-        case 'alert': alert(label + ' ' + key + ' ' + JSON.stringify(event)); break;
-        case 'body': document.body.appendChild(document.createTextNode(label + ' ' + key + ' ' + JSON.stringify(event) + '\n')); break;
-        default: throw new Error(`Unexpected mode '${mode}'.`);
+        case 'log': {
+          console.log(label, key, event);
+          break;
+        }
+        case 'alert': {
+          alert(label + ' ' + key + ' ' + JSON.stringify(event));
+          break;
+        }
+        case 'body': {
+          if (logsP === undefined) {
+            logsP = document.querySelector('#logsP');
+          }
+          
+          logsP.appendChild(document.createTextNode(label + ' ' + key + ' ' + JSON.stringify(event) + '\n'));
+          break;
+        }
+        default: {
+          throw new Error(`Unexpected mode '${mode}'.`);
+        }
       }
     });
   }
