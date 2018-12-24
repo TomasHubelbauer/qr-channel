264800e064d80864c5228369384fdff537155677
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 11:06:00 +0100

    Skip the visual stuff while debugging the test rig in iOS Safari

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import test from './test.js';
 
 window.addEventListener('load', async () => {
   await test();
+  return;
   
   const signalingStateP = document.querySelector('#signalingStateP');
   const iceGatheringStateP = document.querySelector('#iceGatheringStateP');
