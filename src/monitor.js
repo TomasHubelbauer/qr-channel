ae672686024d2b113023842c68cfa59c1d7c1385
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 11:11:32 +0100

    Create a new 'body' mode for logging

--- src/monitor.js
+++ src/monitor.js
@@ -1,4 +1,4 @@
-const mode = 'alert';
+const mode = 'body';
 export default function monitor(obj, label) {
   for (const key in obj) {
     if (!/^on/.test(key)) {
@@ -9,6 +9,7 @@ export default function monitor(obj, label) {
       switch (mode) {
         case 'log': console.log(label, key, event); break;
         case 'alert': alert(label + ' ' + key + ' ' + JSON.stringify(event)); break;
+        case 'body': document.body.appendChild(document.createTextNode(label + ' ' + key + ' ' + JSON.stringify(event) + '\n')); break;
         default: throw new Error(`Unexpected mode '${mode}'.`);
       }
     });
