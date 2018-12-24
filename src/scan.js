a70490675e2f6d62571b2b4b65259333d96d78ce
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 10:49:15 +0100

    Move completing the message into the scanner

--- src/scan.js
+++ src/scan.js
@@ -1,8 +1,10 @@
 // TODO: Change this to an async iterator once browser support is there https://github.com/Fyrd/caniuse/issues/3690
-export default function scan(onChunk) {
+export default function scan(onMessage) {
   const viewfinderVideo = document.querySelector('#viewfinderVideo');
   const viewfinderCanvas = document.querySelector('#viewfinderCanvas');
+  const chunksP = document.querySelector('#chunksP');
   let viewfinderContext;
+  const chunks = [];
   window.requestAnimationFrame(function recognize() {
     if (viewfinderVideo.readyState === viewfinderVideo.HAVE_ENOUGH_DATA) {
       if (viewfinderCanvas.width !== viewfinderVideo.videoWidth || viewfinderCanvas.height !== viewfinderVideo.videoHeight) {
@@ -15,8 +17,22 @@ export default function scan(onChunk) {
       const imageData = viewfinderContext.getImageData(0, 0, viewfinderCanvas.width, viewfinderCanvas.height);
       const code = jsQR(imageData.data, imageData.width, imageData.height);
       if (code !== null && code.data !== '') {
-        const [index, count, text] = code.data.split('\0', 3);
-        onChunk(Number(index), Number(count), text);
+        const [indexStr, countStr, text] = code.data.split('\0', 3);
+        const index = Number(indexStr);
+        const count = Number(countStr);
+        if (chunks[index] === undefined) {
+          chunks[index] = text;
+          // Check the number of keys to ignore uninitialized array items
+          if (Object.keys(chunks).length === count) {
+            const message = chunks.join('');
+            onMessage(message);
+          }
+        } else if (chunks[index] !== text) {
+          // Reset scanning as we've found different chunks at the same index, the peer has probably changed
+          chunks = [];
+        }
+        
+        chunksP.textContent = `Chunks (${Object.keys(chunks).length}/${count}): ` + Object.keys(chunks);
       }
     }
 
