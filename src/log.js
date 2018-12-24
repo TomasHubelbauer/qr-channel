8162435dbf57da404a4cbc9e4f080431cb29f262
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 11:38:45 +0100

    Pull out the log function to stand on its own

---
+++ src/log.js
@@ -0,0 +1,25 @@
+const mode = 'body';
+let logsP;
+export default function log(message) {
+  switch (mode) {
+    case 'log': {
+      console.log(message);
+      break;
+    }
+    case 'alert': {
+      alert(message);
+      break;
+    }
+    case 'body': {
+      if (logsP === undefined) {
+        logsP = document.querySelector('#logsP');
+      }
+
+      logsP.appendChild(document.createTextNode(message + '\n'));
+      break;
+    }
+    default: {
+      throw new Error(`Unexpected mode '${mode}'.`);
+    }
+  }
+}
