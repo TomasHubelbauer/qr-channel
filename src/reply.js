556498f2cbf171340572d417edc4e23b37468f88
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 21:59:17 +0100

    Start drafting up the reply logic

---
+++ src/reply.js
@@ -0,0 +1,25 @@
+import decode from './decode.js';
+
+export default function reply(message) {
+  if (message.startsWith('a=candidate:')) {
+    throw new Error('TODO: Handle candidates');
+  }
+  
+  const sessionDescription = decode(message);
+  switch (sessionDescription.type) {
+    case 'offer': {
+      const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.services.mozilla.com' } ] });
+      monitor(peerConnection, 'peerConnection');
+      console.log('answering someone elses offer');
+      // TODO: Discard my own offer
+      break;
+    }
+    case 'answer': {
+      throw new Error('TODO: Implement handling answer to my own offer');
+      break;
+    }
+    default: {
+      throw new Error(`Unexpected session description type '${sessionDescription.type}'.`);
+    }
+  }
+}
