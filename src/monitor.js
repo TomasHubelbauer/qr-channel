e8a9fe750a452d3ff1938c68c9d927b966ab8558
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 11:08:31 +0100

    Expand the monitor to support log and alert modes

--- src/monitor.js
+++ src/monitor.js
@@ -1,9 +1,16 @@
+const mode = 'alert';
 export default function monitor(obj, label) {
   for (const key in obj) {
     if (!/^on/.test(key)) {
       continue;
     }
 
-    obj.addEventListener(key.slice(2), event => alert(label, key, event));
+    obj.addEventListener(key.slice(2), event => {
+      switch (mode) {
+        case 'log': console.log(label, key, event); break;
+        case 'alert': alert(label + ' ' + key + ' ' + JSON.stringify(event)); break;
+        default: throw new Error(`Unexpected mode '${mode}'.`);
+      }
+    });
   }
 }
