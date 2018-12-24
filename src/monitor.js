a0079704889d54bf790b6a7d835f4acbaa3fd6e0
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-24 11:38:13 +0100

    Use the log function for monitoring

--- src/monitor.js
+++ src/monitor.js
@@ -1,33 +1,11 @@
-const mode = 'body';
-let logsP;
+import log from './log.js';
+
 export default function monitor(obj, label) {
   for (const key in obj) {
     if (!/^on/.test(key)) {
       continue;
     }
 
-    obj.addEventListener(key.slice(2), event => {
-      switch (mode) {
-        case 'log': {
-          console.log(label, key, event);
-          break;
-        }
-        case 'alert': {
-          alert(label + ' ' + key + ' ' + JSON.stringify(event));
-          break;
-        }
-        case 'body': {
-          if (logsP === undefined) {
-            logsP = document.querySelector('#logsP');
-          }
-          
-          logsP.appendChild(document.createTextNode(label + ' ' + key + ' ' + JSON.stringify(event) + '\n'));
-          break;
-        }
-        default: {
-          throw new Error(`Unexpected mode '${mode}'.`);
-        }
-      }
-    });
+    obj.addEventListener(key.slice(2), event => log(label + ' ' +key));
   }
 }
