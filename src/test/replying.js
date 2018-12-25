b3cba06f43edebe3fb01c77f7625b5278c42613d
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-26 00:01:00 +0100

    Listen for passed data and push it forth for processing

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,5 +1,9 @@
-export default async function replying() {
+export default async function replying(onMessage) {
   const codeCanvas = document.querySelector('#codeCanvas');
+  window.addEventListener('message', event => {
+    onMessage(event.data);
+  });
+  
   while (true) {
     await new Promise(resolve => window.setTimeout(resolve, 100));
     const message = codeCanvas.title;
