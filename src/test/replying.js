75e8ab49b968115c8e80e914461802d6ae87c111
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 19:47:28 +0100

    Read canvas title instead of recognizing graphically
    This should still ensure that if the code breaks, the test breaks, because the QR code image and the title content are sourced from the same variable.

--- src/test/replying.js
+++ src/test/replying.js
@@ -2,17 +2,7 @@ export default async function replying(onReply) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
     const canvas = document.createElement('canvas');
-    canvas.width = codeCanvas.width + 100;
-    canvas.height = codeCanvas.height + 100;
-    const context = canvas.getContext('2d');
-    context.drawImage(codeCanvas, 50, 50);
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    
-    const code = jsQR(imageData.data, imageData.width, imageData.height);
-    console.log(canvas.toDataURL(), code);
-    if (code !== null && code.data !== '') {
-      // TODO: Interpret the code given own state and reply using `onReply`
-    }
+    console.log(canvas.title);
     
     await new Promise(resolve => window.setTimeout(resolve, 1000));
   }
