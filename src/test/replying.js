fc4857decfc4f69278518372a73220822846a1bc
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:57:26 +0100

    Try a different phase

--- src/test/replying.js
+++ src/test/replying.js
@@ -11,7 +11,7 @@ export default async function replying(onReply) {
     // Deep fry the image, maybe it needs to be more photo-like to get recognized? ¯\_(ツ)_/¯
     // https://github.com/TomasHubelbauer/canvas-chromatic-abberation
     const intensity = 5;
-    const phase = 0;
+    const phase = 2;
     for (let i = phase % 4; i < imageData.data.length; i += 4) {
       // Setting the start of the loop to a different integer will change the aberration color, but a start integer of 4n-1 will not work
       imageData.data[i] = imageData.data[i + 4 * intensity];
