2b63a64906b2a8c93d8c0985be8ccc4fea1d9c3f
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 19:53:18 +0100

    Fix up the syntax where the message was not passed to the callback

--- src/test/replying.js
+++ src/test/replying.js
@@ -2,7 +2,6 @@ export default async function replying(onMessage) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
     onMessage(codeCanvas.title);
-    
     await new Promise(resolve => window.setTimeout(resolve, 1000));
   }
 }
