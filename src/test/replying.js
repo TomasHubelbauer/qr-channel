96415b323fae9abdd83e8e84255ce3d1fddf0988
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 23:03:25 +0100

    Postpone the message passing  to give ICE time to come

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,12 +1,13 @@
 export default async function replying(onMessage) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
-    await new Promise(resolve => window.setTimeout(resolve, 1000));
+    await new Promise(resolve => window.setTimeout(resolve, 100));
     const message = codeCanvas.title;
     if (message === '') {
       continue;
     }
     
+    window.setTimeout(onMessage, 2500, codeCanvas.title);
     onMessage(codeCanvas.title);
   }
 }
