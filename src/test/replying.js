2732479f57e5edb9496a028104d04078cd7e8b04
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:23:49 +0100

    Add spacing around the QR code to aid recognition

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,10 +1,14 @@
 export default async function replying(onReply) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
-    const context = codeCanvas.getContext('2d');
-    const imageData = context.getImageData(0, 0, codeCanvas.width, codeCanvas.height);
+    const canvas = document.createElement('canvas');
+    canvas.width = codeCanvas.width + 10;
+    canvas.height = codeCanvas.height + 10;
+    const context = canvas.getContext('2d');
+    context.drawImage(codeCanvas, 5, 5);
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     const code = jsQR(imageData.data, imageData.width, imageData.height);
-    console.log(code);
+    console.log(canvas.toBaseURI(), code);
     if (code !== null && code.data !== '') {
       // TODO: Interpret the code given own state and reply using `onReply`
     }
