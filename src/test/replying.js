313db9ca25976440153228a2bd49930efbe1fb8e
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 18:00:30 +0100

    Remove the chromatic abberation gimmick, didn't work

--- src/test/replying.js
+++ src/test/replying.js
@@ -8,22 +8,6 @@ export default async function replying(onReply) {
     context.drawImage(codeCanvas, 50, 50);
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     
-    // Deep fry the image, maybe it needs to be more photo-like to get recognized? ¯\_(ツ)_/¯
-    // https://github.com/TomasHubelbauer/canvas-chromatic-abberation
-    const intensity = 5;
-    const phase = Math.floor(Math.random() * 5);
-    for (let i = phase % 4; i < imageData.data.length; i += 4) {
-      // Setting the start of the loop to a different integer will change the aberration color, but a start integer of 4n-1 will not work
-      imageData.data[i] = imageData.data[i + 4 * intensity];
-    }
-    
-    context.putImageData(imageData, 0, 0);
-    
-    const viewfinderCanvas = document.querySelector('#viewfinderCanvas');
-    viewfinderCanvas.style.display = 'initial';
-    const viewfinderContext = viewfinderCanvas.getContext('2d');
-    viewfinderContext.putImageData(imageData, 0, 0);
-    
     const code = jsQR(imageData.data, imageData.width, imageData.height);
     console.log(canvas.toDataURL(), code);
     if (code !== null && code.data !== '') {
