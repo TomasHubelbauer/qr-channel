b86235c8fd342b8713a2067016c45fa7a4671168
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 19:56:42 +0100

    Ignore empty message (happens before any codes are shown)

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,8 +1,11 @@
 export default async function replying(onMessage) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
-    console.log(JSON.stringify(codeCanvas.title));
-    onMessage(codeCanvas.title);
     await new Promise(resolve => window.setTimeout(resolve, 1000));
+    const message = codeCanvas.title;
+    if (message === '') {
+      continue;
+    }
+    onMessage(codeCanvas.title);
   }
 }
