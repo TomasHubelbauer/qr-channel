6921cf32294d3720e70e56f86e2d445ec4f13078
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:50:37 +0100

    Apply chromatic abberation on the image before attempting to recognize

--- src/test/replying.js
+++ src/test/replying.js
@@ -7,6 +7,18 @@ export default async function replying(onReply) {
     const context = canvas.getContext('2d');
     context.drawImage(codeCanvas, 50, 50);
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    
+    // Deep fry the image, maybe it needs to be more photo-like to get recognized? ¯\_(ツ)_/¯
+    // https://github.com/TomasHubelbauer/canvas-chromatic-abberation
+    const intensity = 5;
+    const phase = 0;
+    for (let i = phase % 4; i < imageData.data.length; i += 4) {
+      // Setting the start of the loop to a different integer will change the aberration color, but a start integer of 4n-1 will not work
+      imageData.data[i] = imageData.data[i + 4 * intensity];
+    }
+    
+    context.putImageData(imageData, 0, 0);
+    
     const code = jsQR(imageData.data, imageData.width, imageData.height);
     console.log(canvas.toDataURL(), code);
     if (code !== null && code.data !== '') {
