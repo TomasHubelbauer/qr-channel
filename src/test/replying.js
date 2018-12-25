a2d18b03aed845bd2dc09c25f53f853b00396d42
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 19:55:14 +0100

    Log the "scanned" message before passing it for debugging

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,6 +1,7 @@
 export default async function replying(onMessage) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
+    console.log(JSON.stringify(codeCanvas.title));
     onMessage(codeCanvas.title);
     await new Promise(resolve => window.setTimeout(resolve, 1000));
   }
