c3c0faefda11b2e949df05d9106c8bba9bc81fbb
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:27:42 +0100

    Increase the spacing to hopefully aid recognition

--- src/test/replying.js
+++ src/test/replying.js
@@ -2,10 +2,10 @@ export default async function replying(onReply) {
   const codeCanvas = document.querySelector('#codeCanvas');
   while (true) {
     const canvas = document.createElement('canvas');
-    canvas.width = codeCanvas.width + 10;
-    canvas.height = codeCanvas.height + 10;
+    canvas.width = codeCanvas.width + 100;
+    canvas.height = codeCanvas.height + 100;
     const context = canvas.getContext('2d');
-    context.drawImage(codeCanvas, 5, 5);
+    context.drawImage(codeCanvas, 50, 50);
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     const code = jsQR(imageData.data, imageData.width, imageData.height);
     console.log(canvas.toDataURL(), code);
