b2eb5124a79dcb51a24cf7e3660f4668d408d273
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 23:19:17 +0100

    Take out the type from the identification
    ICE candidates do not carry it is not worth keeping and complicating the comparison.

--- src/identify.js
+++ src/identify.js
@@ -1,9 +1,7 @@
 export default function identify(sessionDescription) {
-  const sessionId = sessionDescription.sdp
+  return sessionDescription.sdp
     .split(/\r\n/g)
     .map(l => l.match(/^o=.* (\d+) \d+ IN IP4 \d+.\d+.\d+.\d+$/))
     .filter(m => m !== null)
     [0][1];
-  
-  return `${sessionDescription.type} ${sessionId}`;
 }
