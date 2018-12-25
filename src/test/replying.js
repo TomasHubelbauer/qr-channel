3db2d01359b089bb67a0d3d05f18d3736599e172
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:58:37 +0100

    Reveal the canvas and randomize the phase

--- src/test/replying.js
+++ src/test/replying.js
@@ -11,15 +11,17 @@ export default async function replying(onReply) {
     // Deep fry the image, maybe it needs to be more photo-like to get recognized? ¯\_(ツ)_/¯
     // https://github.com/TomasHubelbauer/canvas-chromatic-abberation
     const intensity = 5;
-    const phase = 2;
+    const phase = Math.floor(Math.random() * 5);
     for (let i = phase % 4; i < imageData.data.length; i += 4) {
       // Setting the start of the loop to a different integer will change the aberration color, but a start integer of 4n-1 will not work
       imageData.data[i] = imageData.data[i + 4 * intensity];
     }
     
-    const viewfinderCanvas = document.querySelector('#viewfinderCanvas');
-    const viewfinderContext = viewfinderCanvas.getContext('2d');
     context.putImageData(imageData, 0, 0);
+    
+    const viewfinderCanvas = document.querySelector('#viewfinderCanvas');
+    viewfinderCanvas.style.display = 'initial';
+    const viewfinderContext = viewfinderCanvas.getContext('2d');
     viewfinderContext.putImageData(imageData, 0, 0);
     
     const code = jsQR(imageData.data, imageData.width, imageData.height);
