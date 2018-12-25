0861354d6bde95c0829dcfa5c27786e7d50a8e57
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:04:09 +0100

    Plan the phases of the replying test

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,1 +1,4 @@
-// TODO: Put together the mock reply test here
+export default function replying() {
+  // TODO: Read the QR codes from the code canvas and recognize their content using the library
+  // TODO: Send the values to reply
+}
