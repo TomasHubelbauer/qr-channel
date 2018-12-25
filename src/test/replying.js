fe120343f44a4817f1277cb371ffed3a3d1ba5f4
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:14:08 +0100

    Add the missing line with encoding

--- src/test/replying.js
+++ src/test/replying.js
@@ -3,6 +3,7 @@ export default function replying(onReply) {
   while (true) {
     const context = codeCanvas.getContext('2d');
     const imageData = context.getImageData(0, 0, codeCanvas.width, codeCanvas.height);
+    const code = jsQR(imageData.data, imageData.width, imageData.height);
     if (code !== null && code.data !== '') {
       console.log(code.data);
       // TODO: Interpret the code given own state and reply using `onReply`
