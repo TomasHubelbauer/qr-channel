528fbfc02bfbf314b78863e9a263ef8ec50c03fb
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:11:43 +0100

    Try to see if the library can read the rendered codes

--- src/test/replying.js
+++ src/test/replying.js
@@ -1,4 +1,11 @@
-export default function replying() {
-  // TODO: Read the QR codes from the code canvas and recognize their content using the library
-  // TODO: Send the values to reply
+export default function replying(onReply) {
+  const codeCanvas = document.querySelector('#codeCanvas');
+  while (true) {
+    const context = codeCanvas.getContext('2d');
+    const imageData = context.getImageData(0, 0, codeCanvas.width, codeCanvas.height);
+    if (code !== null && code.data !== '') {
+      console.log(code.data);
+      // TODO: Interpret the code given own state and reply using `onReply`
+    }
+  }
 }
