162c5190c74fc570b2a1a3309a11cac2d3c6d66c
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 09:30:47 +0100

    Clean up the index imports after refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import scan from './scan.js';
-import broadcast from './broadcast.js';
-import monitor from './monitor.js';
 import test from './test.js';
+import scan from './scan.js';
 import reply from './reply.js';
 
 window.addEventListener('load', async () => {
   await test();
   
   // Fire and forget infinite scanning for QR codes
-  scan(async message => await reply(message));
+  scan(reply);
   
   // Start broadcasting my own welcome offer
   reply();
