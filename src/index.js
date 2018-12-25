44022db208b57e0d4abddc975f37cea9e00d40f6
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 09:27:11 +0100

    Invoke the response reply with no message to show the initial welcome offer

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import reply from './reply.js';
 window.addEventListener('load', async () => {
   await test();
   
-  const signalingStateP = document.querySelector('#signalingStateP');
-  const iceGatheringStateP = document.querySelector('#iceGatheringStateP');
-
   scan(async message => await reply(message));
   
   const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
@@ -19,20 +16,7 @@ window.addEventListener('load', async () => {
   // Play through JavaScript, `autoplay` doesn't seem to work
   await viewfinderVideo.play();
 
-  const peerConnection = new RTCPeerConnection({ iceServers: [ { urls: 'stun:stun.services.mozilla.com' } ] });
-  monitor(peerConnection, 'peerConnection');
-
-  peerConnection.addEventListener('signalingstatechange', () => signalingStateP.textContent += peerConnection.signalingState + '; ');
-  peerConnection.addEventListener('icegatheringstatechange', () => iceGatheringStateP.textContent += peerConnection.iceGatheringState + '; ');
-
-  const dataChannel = peerConnection.createDataChannel(null);
-  monitor(dataChannel, 'dataChannel');
-
-  const sessionDescription = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(sessionDescription);
-
-  // Start rotating now that we have SDP
-  broadcast(peerConnection);
+  reply();
 });
 
 window.addEventListener('unhandledrejection', event => alert(event.reason));
