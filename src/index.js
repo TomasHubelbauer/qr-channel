164c640170c89e02c3e29c3e7614e7b0ccfa8f7c
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-25 17:02:10 +0100

    Fix path to the coding test and run the replying test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import test from './test.js';
+import codingTest from './test/coding.js';
+import replyingTest from './test/replying.js';
 import scan from './scan.js';
 import reply from './reply.js';
 
 window.addEventListener('load', async () => {
-  //await test();
+  //await codingTest();
   
   // Fire and forget infinite scanning for QR codes
-  scan(reply);
+  //scan(reply);
   
   // Start broadcasting my own welcome offer
   reply();
+  
+  // Run the replying test
+  replyingTest(reply);
 });
 
 window.addEventListener('unhandledrejection', event => alert(event.reason));
