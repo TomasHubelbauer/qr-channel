0ec54a878f560ec715c3752d2369235283db61a6
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-12-26 10:27:58 +0100

    Return the session ID with the candidate

--- src/melt.js
+++ src/melt.js
@@ -2,6 +2,6 @@ export default function melt(ice, sdp) {
   const [sdp, id] = message.split('\n');
   
   if (!sdp.sdp.split(/\r\n/g).includes(sdp)) {
-    return new RTCIceCandidate({ candidate: sdp, sdpMid: "0", sdpMLineIndex: 0 });
+    return { sdp: new RTCIceCandidate({ candidate: sdp, sdpMid: "0", sdpMLineIndex: 0 }), id };
   }
 }
